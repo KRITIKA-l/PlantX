47b85e4fd5c4767dd17fa20bb839e3c48b32622a
KRITIKA-l <kritika0925@gmail.com>
2025-07-11 11:26:13 +0530

    update

--- plant/views.py
+++ plant/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'plant/home.html')
+
+def explore(request):
+    return render(request,'plant/explore.html')
+
+def myplants(request):
+    return render(request,'plant/myplants.html')
--- project2/urls.py
+++ project2/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from plant import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('explore',views.explore,name='explore'),
+    path('myplants',views.myplants,name='myplants'),
 ]
