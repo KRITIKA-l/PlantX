99c0d5eb148dd733355772a7554739067a9888c8
KRITIKA-l <kritika0925@gmail.com>
2025-07-14 04:44:30 +0530

    Resolved merge conflict in urls.py

--- project2/urls.py
+++ project2/urls.py
@@ -38,8 +38,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-<<<<<<< HEAD
-# your local version
-=======
-# remote version from GitHub
->>>>>>> origin/main
